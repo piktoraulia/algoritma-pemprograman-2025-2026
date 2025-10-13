d09283fb114b6e9ccc6bdb4135a77ddbb69da640
piktoraulia <piktoraulia3@gmail.com>
2025-10-13 20:19:27 +0700

    Create soal1.cpp

---
+++ c/c++/soal1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using name space std;
+
+int main(){
+    string nama ="piktor aulia"
+    int umur =20;
+    float tinggi =170
+    bool aktif =true;
+
+    cout<<"nama:"<<nama<<endl;
+    cout<<"umur:"<<umur<<endl;
+    cout<<"tinggi"<<tinggi<<endl;
+    cout<<"status aktif:"<<aktif<<endl;
+    return 0;
+}
