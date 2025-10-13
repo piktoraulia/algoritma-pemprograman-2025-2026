5312d270ffe18f12962a27a0b31dc3f072afddf5
piktoraulia <piktoraulia3@gmail.com>
2025-10-13 20:20:00 +0700

    Create main.java

---
+++ java/main.java
@@ -0,0 +1,12 @@
+public class main {
+public static void main (string[]args){
+    string nama ="piktor";
+    int umur = 20;
+    double tinggi =170;
+    boolean aktif =true;
+    
+    
+    System.out.println("nama: " + nama);
+    System.out.println("umur: " + umur);
+    System.out.println("tinggi: " + tinggi);
+    System.out.println("status aktif " + aktif);
