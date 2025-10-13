0778a757e27c038e3befe6a014dba99f7248bb87
Moch. Fajar Hikmatulloh <m.fajar.hikmatulloh@unida.ac.id>
2025-10-13 09:48:52 +0700

    first commit

---
+++ c/c++/coba1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+    int a = 7;
+    int b = 2;
+    chor c{15} = "muhamad encep"
+
+    puts("nama");
+    puts(c);
+    printf("terdiri dari %d hurup dan %d kata" ,a,b);
+
+}
