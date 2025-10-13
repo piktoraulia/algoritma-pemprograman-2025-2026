59e49d9dc809434ea99270c0c76ed932cc34700c
piktoraulia <piktoraulia3@gmail.com>
2025-10-13 20:20:05 +0700

    Create soal1.java

---
+++ java/soal1.java
@@ -0,0 +1,11 @@
+import java.util scanner;
+public class main{
+    public static void main(string[]args){
+        final double PI = 3.14159;
+        scaner sc =new scanner(system.in);
+        system.out.print("masukan jari-jari: ");
+        double r = sc.nextDouble();
+        double luasn = PI * r * r ;
+        System.out.println("luas lingkaran=" + luas);
+}
+}
