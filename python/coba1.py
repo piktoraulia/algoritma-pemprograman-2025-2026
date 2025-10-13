1dec84cf77926b049629b01d284f3fa7dd186dae
piktoraulia <piktoraulia3@gmail.com>
2025-10-13 10:01:52 +0700

    Update coba1.py

---
+++ python/coba1.py
@@ -0,0 +1,6 @@
+#menginput alas dan tinggi segitiga 
+alas = float (input("input alas :'))
+tinggi=float (input(;input tinggi :'))
+                    
+#hitung luas segitiga
+luas = (alas * tinggi )
