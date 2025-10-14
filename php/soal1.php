5e726f65412f3282be3f59930b0e3712d2e80ad1
piktoraulia <piktoraulia3@gmail.com>
2025-10-14 20:45:14 +0700

    Update soal1.php

---
+++ php/soal1.php
@@ -0,0 +1,11 @@
+<?php 
+$nama = "piktor";
+$umur = 20;
+$tinggi = 170;
+$aktif = true ;
+
+echo "nama:$nama <br>";
+echo "umur:$umur <br>"; 
+echo "tinggi:$tinggi <br>";
+echo "status aktif: $aktif";
+?>
