6bc1901ef8cf2561a3e0da62031e0c41671e3ad7
piktoraulia <piktoraulia3@gmail.com>
2025-10-20 13:16:28 +0700

    Create variabel.cpp
    latihan konstanta,tipe data variabel dalam cpp

---
+++ c/c++/variabel.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+using  namespace std;
+
+main()
+{
+    short a;
+    int b;
+    long c;
+    float d;
+    double e;
+    char data_karakter = 'A';
+    char materil [9] = "variabel";
+
+    a = 10000;
+    b = 1000000;
+    c = 1000000000;
+    d = 3.12;
+    e = 3.12345;
+
+    cout << "---BELAJAR VARIABEL---" << endl;
+    cout << "variabel a : " << a << endl;
+    cout << "variabel b : " << b << endl;
+    cout << "variabel c : " << c << endl;
+    cout << "variabel d : " << d << endl;
+    cout << "variabel e : " << e << endl;
+    cout << "variabel data_karakter : " << data_karakter << endl;
+    cout << "variabel materil : " << materil << endl;
+    cout << endl;
+}
