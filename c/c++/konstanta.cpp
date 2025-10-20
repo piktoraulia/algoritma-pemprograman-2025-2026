f60cb287d41cb9fa7e6987f1a76b2caf26189f87
piktoraulia <piktoraulia3@gmail.com>
2025-10-20 13:17:50 +0700

    Create konstanta.cpp
    latihan konstanta cpp

---
+++ c/c++/konstanta.cpp
@@ -0,0 +1,21 @@
+#incllude <stdio.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+main()
+{
+
+    const double PI = 3.14;
+    const int NILAI_MAX = 100;
+    const char MyChar = 'A';
+    const string MyName = "piktor aulia";
+
+    cout << "--- BELAJAR KONSTANTA---" << emdl;
+    cout << "konstanta PI : " << PI << endl;
+    cout << "konstanta NILAI_MAX : " << NILAI_MAX << endl;
+    cout << "konstanta MyChar : " << MyChar << endl;
+    cout << "konstanta MyName : " << MyName << endl;
+
+    cout <<endl;
+}
