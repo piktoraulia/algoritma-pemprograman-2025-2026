a0f25b03bfe87f4b714e2b71539814babddea164
piktoraulia <piktoraulia3@gmail.com>
2025-11-04 13:48:07 +0700

    Create tes3.py

---
+++ python/tes3.py
@@ -0,0 +1,11 @@
+nama_warung_kopi = "warkop steko"
+nama_pemilik = "irfan"
+asisten = "arfa"
+menu_minuman_favorit = "kopi gula aren"
+menu_makanan_favorit = "french fries"
+
+print ("nama warung kopi", "didirikan tahun 2020 dengan nama", nama_warung_kopi)
+print ("pemilik dan pendiri warung kopi adalah", nama_pemilik, "di daerah pamoyanan")
+print ("asisten warkop steko adalah", asisten, "teman sma irfan")
+print ("menu minuman favorit di warkop seteko adalah", menu_minuman_favorit)
+print ("menu makanan favorit di warkop steko adalah", menu_makanan_favorit)
