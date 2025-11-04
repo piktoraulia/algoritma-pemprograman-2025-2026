3560b64a0cffb010846513d02b59ebd546a5b04f
piktoraulia <piktoraulia3@gmail.com>
2025-11-04 13:48:04 +0700

    Create tes2.py

---
+++ python/tes2.py
@@ -0,0 +1,9 @@
+nama = "piktor aulia"
+kegiatan = "warung kopi"
+tempat_tidur = "kasur raja"
+
+
+
+print ("hallo,", nama, "selamat datang")
+print ("dia mendirikan", kegiatan, "di depan gg keramat")
+print ("dia sering beristirahat di", tempat_tidur)
