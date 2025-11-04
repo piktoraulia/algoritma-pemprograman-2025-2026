eada4cf3ffe2bc3a1bcb86ecefec584d9b9c358c
piktoraulia <piktoraulia3@gmail.com>
2025-11-04 13:47:58 +0700

    Create tes.py

---
+++ python/tes.py
@@ -0,0 +1,17 @@
+data = "data unit fasilitas sekolah"
+kursi = "20 unit"
+meja = "10 unit"
+papan_tulis = "1 unit"
+sapu = "3 unit"
+kain_pel = "2 unit"
+murid = "20 siswa"
+kursi_guru = "1 unit"
+
+print ("data", data)
+print ("jumlah kursi", kursi)
+print ("jumlah meja", meja)
+print ("jumlah papan tulis", papan_tulis)
+print ("jumlah sapu", sapu)
+print ("jumlah kain pel", kain_pel)
+print ("jumlah murid", murid)
+print ("jumlah kursi guru", kursi_guru)
